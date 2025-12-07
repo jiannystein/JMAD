cc20204e957b6e79c44188e738fcfcda86800d6d
jiannyshawnlaptop <41028418+jiannystein@users.noreply.github.com>
2025-12-07 14:25:42 +0800

    1

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+console.log("JMAD Website Loaded");
+
+// Future: Add smooth scroll or IntersectionObserver animations here
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
